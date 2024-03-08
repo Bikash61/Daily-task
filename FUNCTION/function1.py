f7681e142458ebebae28050987fa41beaff756f1
Bikash61 <bashyalb96@gmail.com>
2024-03-09 00:22:42 +0545

    Commiting function

---
+++ FUNCTION/function1.py
@@ -0,0 +1,20 @@
+def sum(a,b):
+    s = a +b
+    return s
+print(sum(3,6))
+print(sum(34,23))
+
+def  greet():
+    print("Namaste")
+    print("Hello Everyone ")
+    return 0 
+
+greet()
+sumb = sum(232,23)
+print(sumb)
+
+def week( a,b):
+    diff = a - b
+    print(diff)
+
+week(2,4)
