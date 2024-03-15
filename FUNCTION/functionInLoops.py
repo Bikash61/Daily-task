056a516bb361abb277eead7fd6214a47b8f69f89
Bikash61 <bashyalb96@gmail.com>
2024-03-15 12:03:06 +0545

    updates

---
+++ FUNCTION/functionInLoops.py
@@ -0,0 +1,24 @@
+#def greet():
+#   print("Hello Everyone")
+#    print("Namaste Everyone")
+
+#for i in range (1, 11):
+    #greet()
+
+#write a funcition to calculate the average of 3 numbers 
+
+"""def calc_avg(a,b,c):
+    sum = a +b+c
+    avg = sum/3
+    print(avg)
+    return avg 
+
+calc_avg(6,8,9)"""
+
+cities = [ "Butwal", "Palpa", "Kapilvastu"]
+hero = ["Salman", "Saharukh", "Mahesh"]
+def print_len(list):
+    print(list , end = " ")
+
+print_len(cities)
+print_len(hero)
---
+++ PYTHON BASICS/basic.py
@@ -0,0 +1,5 @@
+first = int (input("Enter the first number : "))
+second = int(input("Enter the second number : "))
+
+sum = first + second
+print("The sum is " , sum )
