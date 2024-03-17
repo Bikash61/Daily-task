a49fa6cc94639dc051fe1eedf4e1e0346db8333a
Bikash61 <bashyalb96@gmail.com>
2024-03-17 11:13:23 +0545

    Commiting updates

--- FUNCTION/usdToInr.py
+++ FUNCTION/usdToInr.py
@@ -1,9 +1,17 @@
 
+#First method
 a = int(input("Enter the dollar you want to convert in INR"))
 print("$", a)
-
 def conversion( a):
-    a = a*80
+    a = a*83.6
     print("INR",a)
-
 conversion(a)
+
+
+#Second Method 
+def converter (usd):
+    inr = usd *83.6
+    print("$", usd, "is equal to", "INR", inr  )
+
+
+converter(3)
