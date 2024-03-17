3bd9983d991d9df254476bc31bbb2e1a567c1507
Bikash61 <bashyalb96@gmail.com>
2024-03-17 11:08:08 +0545

    Updates

--- FUNCTION/factorialFunction.py
+++ FUNCTION/factorialFunction.py
@@ -1,6 +1,6 @@
 
 #WAP to caluclate the factorial of n number 
-a = int(input("Enter the number you want to print?"))
+a = int(input("Enter the number y of factorial you want to print?"))
 
 def factorial(a, fact = 1):
     for i in range(1, a+1):
---
+++ FUNCTION/usdToInr.py
@@ -0,0 +1,9 @@
+
+a = int(input("Enter the dollar you want to convert in INR"))
+print("$", a)
+
+def conversion( a):
+    a = a*80
+    print("INR",a)
+
+conversion(a)
