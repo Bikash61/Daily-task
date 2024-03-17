1e6fea91176b8daff013fff9927a070a3cadf0c3
Bikash61 <bashyalb96@gmail.com>
2024-03-17 10:11:17 +0545

    Commiting updates

---
+++ FUNCTION/practiceFunction.py
@@ -0,0 +1,10 @@
+
+cities = ["Butwal", "Kathmandu", "Palpa", "Parbat", "Manasulu"]
+friends = ["Anurodh", "Bikash", "Ajay", "Abhi", "Manav"]
+
+def print_list(list):
+    for i in list:
+        print(i, end=" " )
+
+print_list(cities)
+print_list(friends)
