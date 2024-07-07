43e02d5fd3fa88ae05104f507dff3e30af5544e8
Bikash61 <bashyalb96@gmail.com>
2024-07-07 10:50:13 +0545

    Commiting updates

---
+++ FUNCTION/BUILTIN MODULES OR LIBRARIES/math.py
@@ -0,0 +1,24 @@
+import math
+x = 10.8
+y = 2.5
+print(math.floor(x))
+print(math.ceil(x))
+print(math.trunc(x))
+
+print('-'*30)
+print(math.exp(y))
+print(math.floor((math.exp(y))))
+print('-'*30)
+
+a = 90
+print(math.sin(a))
+print(math.cos(a))
+print(math.tan(a))
+
+print('-'*30)
+print(math.pi)
+
+print('-'*30)
+
+print(10, math.factorial(10))
+print(5,math.factorial(5))
