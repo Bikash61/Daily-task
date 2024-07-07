f5d0e2796ec7720ec9ae81ce950ee7c44540b5e5
Bikash61 <bashyalb96@gmail.com>
2024-07-07 10:18:13 +0545

    Commiting updates

---
+++ FUNCTION/app.py
@@ -0,0 +1,5 @@
+from support import greet
+greet()
+print('-'*30)
+from support import sum
+sum()
---
+++ FUNCTION/support.py
@@ -0,0 +1,12 @@
+def greet():
+    name = input("What is you name")
+    print ("Hello " + name + " nice to meet you")
+
+def sum():
+    """This function adds two numbers together."""
+    a = int(input("Enter a number:"))
+    b = int(input("Enter another number:"))
+    print(a + b)
+
+# print('-'*20)
+# sum()
