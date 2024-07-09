e310e282b51aebc62b4a6bda4373382da4f48d81
Bikash61 <bashyalb96@gmail.com>
2024-07-09 18:05:45 +0545

    updating

---
+++ CLASSES AND OBJECT/methods.py
@@ -0,0 +1,31 @@
+class greet:
+    name = 'Bikash'
+    age = 20
+    home = 'Nepal'
+    def nep(self):
+        print("Namaste")
+        print(self.name)
+    
+    def eng(self):
+        print("Hello")
+        print(self.name)
+
+    def ind(self):
+        print("Namaskaram")
+        print(self.age)
+    
+    def mgr(self):
+        print("KUlak Vah Chaley")
+        print(self.home)
+
+greeting = greet()
+
+greeting.nep()
+greeting.eng()
+greeting.ind()
+greeting.mgr()
+
+print('-'*40)
+
+#Factorial of a number 
+
