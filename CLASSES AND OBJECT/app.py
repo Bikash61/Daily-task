22168ec900279eab593195195f959690c133d644
Bikash61 <bashyalb96@gmail.com>
2024-07-09 17:31:46 +0545

    Updating

---
+++ CLASSES AND OBJECT/app.py
@@ -0,0 +1,15 @@
+class greet:
+    print("Hello Everyone")
+
+greet()
+
+class person:
+    name = "Bikash"
+    age = 20
+    Rollno = 11222899
+
+details = person()
+
+print(details.name)
+print(details.age)
+print(details.Rollno)
