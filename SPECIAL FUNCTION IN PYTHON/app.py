fb279193ec251baefe156b503a8cb798c3837d66
Bikash61 <bashyalb96@gmail.com>
2024-07-10 18:35:08 +0545

    Updaing

--- SPECIAL FUNCTION IN PYTHON/app.py
+++ SPECIAL FUNCTION IN PYTHON/app.py
@@ -4,3 +4,19 @@ lst1 = ['Bikash','Samir', 'Pragya']
 lst2 = [20,23,19]
 
 print(list(zip(lst1,lst2)))
+
+print('-'*30)
+
+Matrix = [[1,2,3],[4,5,6],[7,8,9]]
+print([list(row) for row in zip(Matrix)])
+print([list(row) for row in zip(*Matrix)])
+print([list(row) for row in zip(*[list(row) for row in zip(*Matrix)])])
+
+print('-'*30)
+
+lst1 = [2,3,5]
+lst2 = [3,4,6]
+
+lst = zip(lst1,lst2)
+
+print([i*j for i,j in lst])
