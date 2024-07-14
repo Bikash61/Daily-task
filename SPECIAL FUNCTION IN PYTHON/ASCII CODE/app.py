a74622bd7e55776cfb2df588f192e53e17cb4837
Bikash61 <bashyalb96@gmail.com>
2024-07-14 09:34:02 +0545

    Updating content

---
+++ SPECIAL FUNCTION IN PYTHON/ASCII CODE/app.py
@@ -0,0 +1,19 @@
+print('A')
+print('-'*59)
+
+#Ord function ord()
+print(ord("A"))
+print(ord('B'))
+print(ord('C'))
+print('-'*59)
+
+#Chr finction chr()
+print(chr(88))
+print(chr(90))
+print(chr(68))
+print('-'*59)
+
+#printing all ASCII code 
+for i in range(33,128):
+    print(i,chr(i))
+print('-'*59)
