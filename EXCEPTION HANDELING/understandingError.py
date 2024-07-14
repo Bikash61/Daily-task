c385f5d32284f166cd38172c7c35d896ffc6324f
Bikash61 <bashyalb96@gmail.com>
2024-07-14 10:32:23 +0545

    Updating content

---
+++ EXCEPTION HANDELING/understandingError.py
@@ -0,0 +1,10 @@
+
+#This is the run time error 
+#print(10/0)
+
+
+# Logically we have to get the sum of a and b but we had done error by a-b and output is not correct and it is logically incorrect 
+def sum(a,b):
+    return a-b
+
+print(sum(3,5))
